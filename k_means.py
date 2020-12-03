c1772f48a371c7f89f4c3641277cb678b3461c6d
FSlataper <f.slataper@gmail.com>
2020-12-03 16:49:58 +0100

    Added the indexes for evaluate clustering

--- k_means.py
+++ k_means.py
@@ -80,3 +80,43 @@ if __name__ == '__main__':
                   'cluster': [str(i) for i in cl['tuple']]})
     sns.lmplot( x="x1", y="x2", data=pl, fit_reg=False, hue='cluster', legend=False)
     plt.show()
+def DB_index(df):
+    df['tuple'] = df['cluster'].map(tuple)
+    clusters = df.groupby(df["tuple"])
+    dispersions = []
+    separations = []
+    for i in clusters.groups.keys():
+        dis = []
+        for m in df["point"][clusters.groups[i]]:
+            dis.append(np.linalg.norm(np.array(i)-np.array(m)))
+        dispersions.append((sum(dis)/len(dis))**0.5)
+        seps = []
+        for r in clusters.groups.keys():
+            seps.append(np.linalg.norm(np.array(i)-np.array(r)))
+        separations.append(seps)
+    Ds = []
+    for e in range(len(dispersions)):
+        SoI = separations[e]
+        Rs = []
+        for t in range(len(SoI)):
+            if SoI[t] != 0:
+                Rs.append((dispersions[e]+dispersions[t]/SoI[t]))
+        Ds.append(max(Rs))
+    return np.average(Ds)
+from scipy.spatial import distance
+def Dunn_index(df):
+    df['tuple'] = df['cluster'].map(tuple)
+    clusters = df.groupby(df["tuple"])
+    big_delta = []
+    small_delta = []
+    for i in clusters.groups.keys():
+        for k in df["point"][clusters.groups[i]]:
+            for m in df["point"][clusters.groups[i]]:
+                big_delta.append(np.linalg.norm(np.array(k)-np.array(m)))
+        s1 = list(df["point"][clusters.groups[i]])
+        for j in clusters.groups.keys():
+            s2 = list(df["point"][clusters.groups[j]])
+            small_delta_prov = distance.cdist(s1,s2).min(axis=1)
+            small_delta.append(min(small_delta_prov))
+        small_delta = list(filter(lambda x: x!=0,small_delta))
+    return min(small_delta)/max(big_delta)
